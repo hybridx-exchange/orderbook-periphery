b2fc1253c40d7ddf0f880e074f5e976283c734f4
cherideal <101du@163.com>
2021-12-01 22:37:17 +0800

    draft 202112012237

---
+++ contracts/interfaces/IHybridRouter.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.6.2;
+
+interface IHybridRouter {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+
+    //创建token买token限价单
+    function buyWithToken(
+        uint amountOffer,
+        uint price,
+        address baseToken,
+        address quoteToken,
+        address to,
+        uint deadline)
+        external
+        returns (uint);
+
+    //创建eth买token限价单
+    function buyWithEth(
+        uint price,
+        address baseToken,
+        address quoteToken,
+        address to,
+        uint deadline)
+        external
+        payable
+        returns (uint);
+
+    //创建token卖为token限价单
+    function sellToken(
+        uint amountOffer,
+        uint price,
+        address baseToken,
+        address quoteToken,
+        address to,
+        uint deadline)
+        external
+        returns (uint);
+
+    //创建eth卖为token限价单
+    function sellEth(
+        uint price,
+        address baseToken,
+        address quoteToken,
+        address to,
+        uint deadline)
+        external
+        payable
+        returns (uint);
+}
